96043f717c406b937ded53e731e6b969c14be382
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-11-26 17:48:03 +0300

    Filter Operations

--- app/src/main/java/com/bengisusahin/sqlitek/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/sqlitek/MainActivity.kt
@@ -10,18 +10,24 @@ class MainActivity : AppCompatActivity() {
 
         try {
             val myDatabase = this.openOrCreateDatabase("Musicians", MODE_PRIVATE,null)
-            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS musicians (name VARCHAR, age INT)")
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS musicians (id INTEGER PRIMARY KEY," +
+                    " name VARCHAR, age INT)")
             //try catch kullandigimiz icin komut hatası yapsak app cokmez
-            myDatabase.execSQL("INSERT INTO musicians (name,age) VALUES('James',50)")
+            //myDatabase.execSQL("INSERT INTO musicians (name,age) VALUES('James',50)")
+            //myDatabase.execSQL("INSERT INTO musicians (name,age) VALUES('Lars',60)")
+            myDatabase.execSQL("INSERT INTO musicians (name,age) VALUES('Kirk',55)")
 
-            val cursor = myDatabase.rawQuery("SELECT * FROM musicians", null)
+            //val cursor = myDatabase.rawQuery("SELECT * FROM musicians WHERE name = 'James'", null)
+            val cursor = myDatabase.rawQuery("SELECT * FROM musicians WHERE name LIKE '%s'", null)
 
             val nameIx = cursor.getColumnIndex("name")
             val ageIx = cursor.getColumnIndex("age")
+            val idIx = cursor.getColumnIndex("id")
 
             while (cursor.moveToNext()){
                 println("Name: " + cursor.getString(nameIx))
                 println("Age: " + cursor.getInt(ageIx))
+                println("Id: " + cursor.getInt(idIx))
             }
             cursor.close()
         } catch (e: Exception){
