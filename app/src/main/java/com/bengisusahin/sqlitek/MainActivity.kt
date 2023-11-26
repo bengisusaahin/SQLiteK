67276fa97e5caa6ad9d7e3d6cce42b72caad7c71
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-11-26 16:57:00 +0300

    First registry

--- app/src/main/java/com/bengisusahin/sqlitek/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/sqlitek/MainActivity.kt
@@ -11,6 +11,19 @@ class MainActivity : AppCompatActivity() {
         try {
             val myDatabase = this.openOrCreateDatabase("Musicians", MODE_PRIVATE,null)
             myDatabase.execSQL("CREATE TABLE IF NOT EXISTS musicians (name VARCHAR, age INT)")
+            //try catch kullandigimiz icin komut hatası yapsak app cokmez
+            myDatabase.execSQL("INSERT INTO musicians (name,age) VALUES('James',50)")
+
+            val cursor = myDatabase.rawQuery("SELECT * FROM musicians", null)
+
+            val nameIx = cursor.getColumnIndex("name")
+            val ageIx = cursor.getColumnIndex("age")
+
+            while (cursor.moveToNext()){
+                println("Name: " + cursor.getString(nameIx))
+                println("Age: " + cursor.getInt(ageIx))
+            }
+            cursor.close()
         } catch (e: Exception){
             e.printStackTrace()
         }
