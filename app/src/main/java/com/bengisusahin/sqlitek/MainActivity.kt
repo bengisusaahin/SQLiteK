05adc28eb47b96bd0b5b39aa6ff6f3a0fa970f1e
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-11-26 16:30:21 +0300

    SQLite Database

--- app/src/main/java/com/bengisusahin/sqlitek/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/sqlitek/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        try {
+            val myDatabase = this.openOrCreateDatabase("Musicians", MODE_PRIVATE,null)
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS musicians (name VARCHAR, age INT)")
+        } catch (e: Exception){
+            e.printStackTrace()
+        }
     }
 }
